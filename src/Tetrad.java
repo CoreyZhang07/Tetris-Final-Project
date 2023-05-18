014a6bda3706ffccc23bbf44c77b40bb66cc9dcc
s-haili <s-haili@486E294106.lwsd.k12wa.org>
2023-05-18 09:00:52 -0700

    Working on exercise 12 now

--- src/Tetrad.java
+++ src/Tetrad.java
@@ -15,11 +15,14 @@ import java.awt.Color;
 public class Tetrad
 {
 	private Block[] blocks;	// The blocks for the piece.
+	private BoundedGrid<Block> grid;
 
 	// Constructs a Tetrad.
 	public Tetrad(BoundedGrid<Block> grid)
 	{
 		//throw new RuntimeException("INSERT MISSING CODE HERE");
+		this.grid = grid;
+		
 		blocks = new Block[4];
 		Color tetradColor;
 		Location[] blockLocations;
@@ -28,11 +31,11 @@ public class Tetrad
 		
 		if (tetradRandomizer == 0) { // Red "I"
 			tetradColor = Color.RED;
-			Location[] tempLocations = {new Location(0, 3), new Location(0, 4), new Location(0, 5), new Location(0, 6)};
+			Location[] tempLocations = {new Location(0, 4), new Location(0, 3), new Location(0, 5), new Location(0, 6)};
 			blockLocations = tempLocations;
 		} else if (tetradRandomizer == 1) { // Gray "T"
 			tetradColor = Color.GRAY;
-			Location[] tempLocations = {new Location(0, 3), new Location(0, 4), new Location(0, 5), new Location(1, 4)};
+			Location[] tempLocations = {new Location(0, 4), new Location(0, 3), new Location(0, 5), new Location(1, 4)};
 			blockLocations = tempLocations;
 		} else if (tetradRandomizer == 2) { // Cyan "O"
 			tetradColor = Color.CYAN;
@@ -44,15 +47,15 @@ public class Tetrad
 			blockLocations = tempLocations;
 		} else if (tetradRandomizer == 4) { // Magenta "J"
 			tetradColor = Color.MAGENTA;
-			Location[] tempLocations = {new Location(0, 4), new Location(0, 5), new Location (0, 6), new Location(1, 6)};
+			Location[] tempLocations = {new Location(0, 5), new Location(0, 4), new Location (0, 6), new Location(1, 6)};
 			blockLocations = tempLocations;
 		} else if (tetradRandomizer == 5) { // Blue "S"
 			tetradColor = Color.BLUE;
-			Location[] tempLocations = {new Location(1, 3), new Location(1, 4), new Location(0, 4), new Location(0, 5)};
+			Location[] tempLocations = {new Location(0, 4), new Location(1, 3), new Location(1, 4), new Location(0, 5)};
 			blockLocations = tempLocations;
 		} else { // If tetradRadomizer is 6, then create a green "Z"
 			tetradColor = Color.GREEN;
-			Location[] tempLocations = {new Location(0, 3), new Location(0, 4), new Location(1, 4), new Location(1, 5)};
+			Location[] tempLocations = {new Location(0, 4), new Location(0, 3), new Location(1, 4), new Location(1, 5)};
 			blockLocations = tempLocations;
 		} 
 		
@@ -73,18 +76,20 @@ public class Tetrad
 	public boolean translate(int deltaRow, int deltaCol)
 	{
 		//throw new RuntimeException("INSERT MISSING CODE HERE");
-		Location[] originalTetradLocations = new Location[blocks.length];
 		Location[] newTetradLocations = new Location[blocks.length];
 		for (int i = 0; i < blocks.length; i++) {
 			Location blockLocation = blocks[i].getLocation();
-			originalTetradLocations[i] = blockLocation;
 			newTetradLocations[i] = new Location(blockLocation.getRow() + deltaRow, blockLocation.getCol() + deltaCol);
 		}
+		for (Location check : newTetradLocations) { // Checks if all of the new locations are in the grid
+			if (grid.isValid(check) == false) {
+				return false;
+			}
+		}
 		
-		BoundedGrid<Block> grid = blocks[0].getGrid();
-		removeBlocks();
+		Location[] originalTetradLocations = removeBlocks();
 		
-		boolean success = areEmpty(grid, newTetradLocations);
+		boolean success = (areEmpty(grid, newTetradLocations));
 		if (success) {
 			addToLocations(grid, newTetradLocations);
 		} else {
@@ -98,7 +103,32 @@ public class Tetrad
 	//                Returns true if successful and false otherwise.
 	public boolean rotate()
 	{
-		throw new RuntimeException("INSERT MISSING CODE HERE");
+		//throw new RuntimeException("INSERT MISSING CODE HERE");
+		Location[] newTetradLocations = new Location[blocks.length];
+		
+		int centerRow = blocks[0].getLocation().getRow();
+		int centerCol = blocks[0].getLocation().getCol();
+		for (int i = 0; i < blocks.length; i++) {
+			Location blockLocation = blocks[i].getLocation();
+			int blockRow = blockLocation.getRow();
+			int blockCol = blockLocation.getCol();
+			newTetradLocations[i] = new Location(centerRow - centerCol + blockCol, centerRow + centerCol - blockRow);
+		}
+		for (Location check : newTetradLocations) { // Checks if all of the new locations are in the grid
+			if (grid.isValid(check) == false) {
+				return false;
+			}
+		}
+		
+		Location[] originalTetradLocations = removeBlocks();
+		
+		boolean success = (areEmpty(grid, newTetradLocations));
+		if (success) {
+			addToLocations(grid, newTetradLocations);
+		} else {
+			addToLocations(grid, originalTetradLocations);
+		}
+		return success;
 	}
 
 
@@ -120,11 +150,11 @@ public class Tetrad
 	public Location[] removeBlocks()
 	{
 		//throw new RuntimeException("INSERT MISSING CODE HERE");
-		Location[] oldLocations = new Location[4];
+		Location[] oldLocations = new Location[blocks.length];
 		BoundedGrid<Block> grid = blocks[0].getGrid();
 		for (int i = 0; i < blocks.length; i++) {
 			oldLocations[i] = blocks[i].getLocation();
-			grid.remove(blocks[i].getLocation());
+			blocks[i].removeSelfFromGrid(); // Sets the grid and location of the block to null
 		}
 		
 		return oldLocations;
--- src/Tetris.java
+++ src/Tetris.java
@@ -10,7 +10,7 @@
  */
 
 // Represents a Tetris game.
-public class Tetris
+public class Tetris implements ArrowListener
 {
 	private BoundedGrid<Block> grid;	// The grid containing the Tetris pieces.
 	private BlockDisplay display;		// Displays the grid.
@@ -23,6 +23,7 @@ public class Tetris
 		grid = new BoundedGrid<Block>(20, 10);
 		display = new BlockDisplay(grid);
 		display.setTitle("Tetris");
+		display.setArrowListener(this);
 		
 		activeTetrad = new Tetrad(grid);
 		display.showBlocks();
@@ -31,16 +32,51 @@ public class Tetris
 	// Play the Tetris Game
 	public void play()
 	{
-		throw new RuntimeException("INSERT MISSING CODE HERE");
+		//throw new RuntimeException("INSERT MISSING CODE HERE");
+		while (true) {
+			sleep(1);
+			boolean successfullyDropped = activeTetrad.translate(1, 0);
+			if (successfullyDropped == true) {
+				display.showBlocks();
+			} else {
+				activeTetrad = new Tetrad(grid);
+			}
+		}
 	}
-
+	
+	// Event listeners for the arrow keys
+	public void upPressed() {
+		activeTetrad.rotate();
+		display.showBlocks();
+	}
+	public void downPressed() {
+		activeTetrad.translate(1, 0);
+		display.showBlocks();
+	}
+	public void leftPressed() {
+		activeTetrad.translate(0, -1);
+		display.showBlocks();
+	}
+	public void rightPressed() {
+		activeTetrad.translate(0, 1);
+		display.showBlocks();
+	}
+	public void spacePressed() {
+		
+	}
 
 	// Precondition:  0 <= row < number of rows
 	// Postcondition: Returns true if every cell in the given row
 	//                is occupied; false otherwise.
 	private boolean isCompletedRow(int row)
 	{
-		throw new RuntimeException("INSERT MISSING CODE HERE");
+		//throw new RuntimeException("INSERT MISSING CODE HERE");
+		for (int col = 0; col < grid.getNumCols(); col++) {
+			if (grid.get(new Location(row, col)) == null) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	// Precondition:  0 <= row < number of rows;
@@ -49,7 +85,11 @@ public class Tetris
 	//                every block above row has been moved down one row.
 	private void clearRow(int row)
 	{
-		throw new RuntimeException("INSERT MISSING CODE HERE");
+		//throw new RuntimeException("INSERT MISSING CODE HERE");
+		int cols = grid.getNumCols();
+		for (int removingCol = 0; removingCol < cols; removingCol++) {
+			grid.get(new Location(row, removingCol)).removeSelfFromGrid();
+		}
 	}
 
 	// Postcondition: All completed rows have been cleared.
@@ -81,9 +121,7 @@ public class Tetris
 	{
 		//throw new RuntimeException("INSERT MISSING CODE HERE");
 		// Testing code here
-		int testsToRun = 1;
-		for (int tests = 1; tests <= testsToRun; tests++) {
-			Tetris newGame = new Tetris();
-		}
+		Tetris newGame = new Tetris();
+		newGame.play();
 	}
 }
